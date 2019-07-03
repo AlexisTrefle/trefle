59fe3e863b6de5c9c1e88aec93eb9ffd4a7dd5e7
AlexisTrefle <52454396+AlexisTrefle@users.noreply.github.com>
2019-07-03 16:40:56 +0200

    Update synapse.sh

--- synapse.sh
+++ synapse.sh
@@ -1,1 +1,24 @@
+#Script permettant de lancer le script python app.py de synapse qui va récupérer
+#les mails du compte configurer dans synapse et les transformer en cases sur thehive
 
+#!bin/bash
+
+echo "...On rentre dans le répertoire de synapse"
+cd /PATH_TO_DIRECTORY || exit
+
+echo "...On lance le script app.py de synapse"
+commande= nohup python3.7 app.py &
+&commande || exit
+sleep 5 || exit
+
+echo "...On lance le curl de recupération des mails de l'adresse configurée"
+curl ADRESSE_IP:PORT/ews2case || exit
+sleep 5 || exit
+
+pid=$( ps ax | grep python3.7 | grep -v grep | awk '{print $1}') || exit
+sleep 1 || exit
+
+echo "... On tue le service synapse" || exit
+kill $pid || exit
+
+echo "...Fin du script"
